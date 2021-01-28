cdeb4ea59e69fa7531fd74b56e21c800c49557ab
wangjie <17054214@qq.com>
2021-01-28 15:18:37 +0800

    用户中心微服务改造

---
+++ domain/user/foodie-user-api/src/main/java/com/imooc/user/service/UserService.java
@@ -0,0 +1,31 @@
+package com.imooc.user.service;
+
+
+import com.imooc.user.bo.UserBO;
+import com.imooc.user.pojo.Users;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("user-api")
+public interface UserService {
+
+    /**
+     * 判断用户名是否存在
+     */
+    @GetMapping("user/exists")
+    public boolean queryUsernameIsExist(@RequestParam("username") String username);
+
+    /**
+     * 判断用户名是否存在
+     */
+    @PostMapping("user")
+    public Users createUser(@RequestBody UserBO userBO);
+
+    /**
+     * 检索用户名和密码是否匹配，用于登录
+     */
+    @GetMapping("verify")
+    public Users queryUserForLogin(@RequestParam("username") String username,
+                                   @RequestParam("password") String password);
+
+
+}
